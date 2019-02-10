645bedc4ef863f4ef576390bf89a5f81bf88bf3f
Rameshwari15 <47074669+Rameshwari15@users.noreply.github.com>
2019-02-10 15:17:23 +0530

    Create BirthdayReminder.py

---
+++ BirthdayReminder.py
@@ -0,0 +1,6 @@
+dict={}
+print("Enter your choice",\n,"1.Enter name of your friend",\n,"2.Add your new friend birthday",\n,"3.Exit")
+choice=int(input())
+if (choice == 1):
+				name=input("Enter name of your friend")
+				dict.get(name,"No Data found")
